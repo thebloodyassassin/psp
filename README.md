fc46c480e0c1acdc3eb99b9a5b2dd332d89e9f9d
Mark <dlrow01134@gmail.com>
2013-12-23 13:00:38 -0500

    201323121300Initial

---
+++ README.md
@@ -0,0 +1,14 @@
+PURPLE SWEATER PROGRAMMING
+http://purplesweaterprogramming.com
+
++++++
+
+currently running wordpress
+
++++++
+
+switching to this blog app that will be created with ror (Ruby on Rails)
+
++++++
+
+MARK B. 2013
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Psp::Application.config.secret_key_base = 'd86425431aaeff98c0fb24518dca1311d279310b4be7bf3d85d611aeacd694cddae1dc18b117455a077c867dff4e354cea7f9afa0fe9e888f92fb439bcfb121a'
